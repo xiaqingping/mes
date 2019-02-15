465047a19bba1374193d5d4e4c09a85b33446f27
Meng YuCheng <maxmeng93@gmail.com>
2019-02-15 14:15:17 +0800

    update

--- config/router.config.js
+++ config/router.config.js
@@ -1,77 +1,77 @@
 export default [
-  // {
-  //   path: '/user',
-  //   component: '../layouts/UserLayout',
-  //   routes: [
-  //     { path: '/user', redirect: '/user/login' },
-  //     { path: '/user/login', component: './User/Login' },
-  //     { path: '/user/register', component: './User/Register' },
-  //   ],
-  // },
-  // {
-  //   path: '/',
-  //   component: '../layouts/BasicLayout',
-  //   routes: [
-  //     // dashboard
-  //     { path: '/', redirect: '/product/categorys' },
-  //     // 产品
-  //     {
-  //       name: 'product',
-  //       icon: 'user',
-  //       path: '/product',
-  //       routes: [
-  //         {
-  //           path: '/product/categorys',
-  //           name: 'categorys',
-  //           component: './Product/Categorys',
-  //         },
-  //         {
-  //           path: '/product/extends',
-  //           name: 'extends',
-  //           component: './Product/Extends',
-  //         },
-  //         {
-  //           path: '/product/promotions',
-  //           name: 'promotions',
-  //           component: './Product/Promotions',
-  //         },
-  //       ],
-  //     },
-  //     // 测序
-  //     {
-  //       name: 'seq',
-  //       icon: 'user',
-  //       path: '/seq',
-  //       authority: ['cx'],
-  //       routes: [
-  //         {
-  //           path: '/seq/order',
-  //           name: 'order',
-  //           component: './Seq/Order',
-  //         },
-  //       ],
-  //     },
-  //     // 系统管理
-  //     {
-  //       name: 'admin',
-  //       icon: 'user',
-  //       path: '/admin',
-  //       routes: [
-  //         {
-  //           path: '/admin/user',
-  //           name: 'user',
-  //           component: './Admin/User',
-  //         },
-  //         {
-  //           path: '/admin/source',
-  //           name: 'source',
-  //           component: './Admin/Source',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       component: '404',
-  //     },
-  //   ],
-  // },
+  {
+    path: '/user',
+    component: '../layouts/UserLayout',
+    routes: [
+      { path: '/user', redirect: '/user/login' },
+      { path: '/user/login', component: './User/Login' },
+      { path: '/user/register', component: './User/Register' },
+    ],
+  },
+  {
+    path: '/',
+    component: '../layouts/BasicLayout',
+    routes: [
+      // dashboard
+      { path: '/', redirect: '/product/categorys' },
+      // 产品
+      {
+        name: 'product',
+        icon: 'user',
+        path: '/product',
+        routes: [
+          {
+            path: '/product/categorys',
+            name: 'categorys',
+            component: './Product/Categorys',
+          },
+          {
+            path: '/product/extends',
+            name: 'extends',
+            component: './Product/Extends',
+          },
+          {
+            path: '/product/promotions',
+            name: 'promotions',
+            component: './Product/Promotions',
+          },
+        ],
+      },
+      // 测序
+      {
+        name: 'seq',
+        icon: 'user',
+        path: '/seq',
+        authority: ['cx'],
+        routes: [
+          {
+            path: '/seq/order',
+            name: 'order',
+            component: './Seq/Order',
+          },
+        ],
+      },
+      // 系统管理
+      {
+        name: 'admin',
+        icon: 'user',
+        path: '/admin',
+        routes: [
+          {
+            path: '/admin/user',
+            name: 'user',
+            component: './Admin/User',
+          },
+          {
+            path: '/admin/source',
+            name: 'source',
+            component: './Admin/Source',
+          },
+        ],
+      },
+      {
+        component: '404',
+      },
+    ],
+  },
 ];
