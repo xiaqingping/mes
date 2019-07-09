b84da659ec20e529914b39fb8167affca17af8c2
Meng YuCheng <maxmeng93@gmail.com>
2019-07-09 16:05:04 +0800

    测序模块下所有的s-table换为vxe-table

--- src/views/seq/order.vue
+++ src/views/seq/order.vue
@@ -1,96 +1,175 @@
 <!-- 测序订单 -->
 <template>
-  <div class="page">
-    <div class="page-content">
-      <s-table
-        size="small"
-        :scroll="{ x: 6000 }"
-        :columns="columns"
-        :data="loadData"
-        :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
-      >
-      </s-table>
+  <div class="page-content">
+
+    <div class="table-search">
+      <a-form layout="inline" :form="form" @submit.prevent="handleSearch">
+        <a-row :gutter="24">
+          <a-col :md="6" :xl="4">
+            <a-form-item label="编号">
+              <a-input v-decorator="['code']"/>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :xl="4">
+            <a-form-item label="名称">
+              <a-input v-decorator="['name']"/>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :xl="4">
+            <a-form-item label="别名">
+              <a-input v-decorator="['alias']"/>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :xl="4">
+            <a-form-item label="系列">
+              <a-select v-decorator="['seriesId', {initialValue: ''}]">
+                <a-select-option value="">全部</a-select-option>
+                <a-select-option v-for="series in $store.state.seq.series" :value="series.id" :key="series.id">{{ series.name }}</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :xl="4">
+            <a-form-item label="状态">
+              <a-select v-decorator="['status', {initialValue: 1}]">
+                <a-select-option value="">全部</a-select-option>
+                <a-select-option v-for="status in $store.state.basic.status" :value="status.id" :key="status.id">{{ status.name }}</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-button type="primary" icon="search" html-type="submit" style="display:none;">查询</a-button>
+      </a-form>
     </div>
+
+    <div class="table-operator">
+      <a-button-group>
+        <a-button icon="search" @click="handleSearch">查询</a-button>
+        <a-button icon="plus" type="primary">新建</a-button>
+      </a-button-group>
+    </div>
+
+    <vxe-grid
+      highlight-hover-row
+      auto-resize
+      height="600"
+      ref="orderTable"
+      :loading="orderTable.loading"
+      :columns="orderTable.columns"
+      :pager-config="orderTable.pagerConfig"
+      :data.sync="orderTable.tableData"
+      @page-change="({pageSize, currentPage}) => this.$utils.tablePageChange({pageSize, currentPage, table: orderTable, callback: handleSearch})">
+    </vxe-grid>
   </div>
 </template>
 
 <script>
-import STable from '@/components/Table';
-
 export default {
-  name: 'SeqSampleOrder',
+  name: 'SeqOrder',
   components: {
-    STable
   },
   data () {
     return {
-      columns: [
-        { title: '订单编号', dataIndex: 'code' },
-        { title: '状态', dataIndex: 'status' },
-        { title: '备注', dataIndex: 'remark' },
-        { title: '客户', dataIndex: 'customerName' },
-        { title: '负责人', dataIndex: 'subcustomerName' },
-        { title: '订货人', dataIndex: 'contactName' },
-        { title: '订货人邮箱', dataIndex: 'contactEmail' },
-        { title: '订货人手机', dataIndex: 'contactMobile' },
-        { title: '创建人姓名', dataIndex: 'creatorName' },
-        { title: '创建时间', dataIndex: 'createDate' },
-        { title: '审核人姓名', dataIndex: 'checkName' },
-        { title: '审核时间', dataIndex: 'checkDate' },
-        { title: '产品金额', dataIndex: 'productAmount' },
-        { title: '订单金额', dataIndex: 'amount' },
-        { title: '销售员', dataIndex: 'salerName' },
-        { title: '修改人姓名', dataIndex: 'changerName' },
-        { title: '修改时间', dataIndex: 'changeDate' },
-        { title: '来源单号', dataIndex: 'sourceCode' },
-        { title: '运费', dataIndex: 'freight' },
-        { title: '是否拼接', dataIndex: 'isSplice' },
-        { title: '样品返还', dataIndex: 'sampleReturn' },
-        { title: '币种', dataIndex: 'currency' },
-        { title: '订单类型', dataIndex: 'orderType' },
-        { title: '测序点', dataIndex: 'seqfactoryId' },
-        { title: '测序中心', dataIndex: 'factory' },
-        { title: '销售组织', dataIndex: 'rangeOrganization' },
-        { title: '分销渠道', dataIndex: 'rangeChannel' },
-        { title: '销售大区', dataIndex: 'regionCode' },
-        { title: '销售网点', dataIndex: 'officeCode' },
-        { title: '开票方式', dataIndex: 'invoiceType' },
-        { title: '付款方式', dataIndex: 'paymentMethod' },
-        { title: '付款条件', dataIndex: 'paymentTerm' },
-        { title: '随货开票', dataIndex: 'invoiceByGoods' },
-        { title: 'SAP销售订单号', dataIndex: 'sapOrderCode' },
-        { title: 'SAP交货单号', dataIndex: 'sapDeliveryCode' },
-        { title: '交货方式', dataIndex: 'deliveryType' },
-        { title: '仓库', dataIndex: 'storageCode' },
-        { title: '客户订单号', dataIndex: 'customerPocode' },
-        { title: '是否有明细', dataIndex: 'isDetails' },
-        { title: '收样网点', dataIndex: 'sampleOfficeCode' },
-        { title: '送货地址', dataIndex: 'address' },
-        { title: '作废人姓名', dataIndex: 'cancelName' },
-        { title: '作废时间', dataIndex: 'cancelDate' },
-        { title: '完成时间', dataIndex: 'finishDate' },
-        { title: '完成人姓名', dataIndex: 'finishName' }
-      ],
+      form: this.$form.createForm(this),
       queryParam: {},
-      loadData: parameter => {
-        const params = Object.assign(parameter, this.queryParam);
-        return this.$api.seqorder.getOrderList(params).then(res => {
-          return {
-            data: res.rows,
-            page: params.page,
-            total: res.total
-          };
-        });
-      },
-      selectedRowKeys: [],
-      selectedRows: []
+      orderTable: {
+        id: 0,
+        xTable: null,
+        loading: false,
+        tableData: [],
+        columns: [],
+        pagerConfig: {
+          currentPage: 1,
+          pageSize: 10,
+          total: 0
+        }
+      }
     };
   },
-  mounted () {},
+  mounted () {
+    this.setColumn();
+    this.handleSearch();
+  },
   methods: {
-    onSelectChange (selectedRowKeys, selectedRows) {
-      this.selectedRowKeys = selectedRowKeys;
-      this.selectedRows = selectedRows;
+    // 设置表格列属性
+    setColumn () {
+      const tableName = 'orderTable';
+      // const { formatter } = this.$utils;
+      // const { basic, seq } = this.$store.state;
+
+      const columns = [
+        { type: 'radio', width: 40 },
+        { type: 'index', width: 40 },
+        { title: '订单编号', field: 'code' },
+        { title: '状态', field: 'status' },
+        { title: '备注', field: 'remark' },
+        { title: '客户', field: 'customerName' },
+        { title: '负责人', field: 'subcustomerName' },
+        { title: '订货人', field: 'contactName' },
+        { title: '订货人邮箱', field: 'contactEmail' },
+        { title: '订货人手机', field: 'contactMobile' },
+        { title: '创建人姓名', field: 'creatorName' },
+        { title: '创建时间', field: 'createDate' },
+        { title: '审核人姓名', field: 'checkName' },
+        { title: '审核时间', field: 'checkDate' },
+        { title: '产品金额', field: 'productAmount' },
+        { title: '订单金额', field: 'amount' },
+        { title: '销售员', field: 'salerName' },
+        { title: '修改人姓名', field: 'changerName' },
+        { title: '修改时间', field: 'changeDate' },
+        { title: '来源单号', field: 'sourceCode' },
+        { title: '运费', field: 'freight' },
+        { title: '是否拼接', field: 'isSplice' },
+        { title: '样品返还', field: 'sampleReturn' },
+        { title: '币种', field: 'currency' },
+        { title: '订单类型', field: 'orderType' },
+        { title: '测序点', field: 'seqfactoryId' },
+        { title: '测序中心', field: 'factory' },
+        { title: '销售组织', field: 'rangeOrganization' },
+        { title: '分销渠道', field: 'rangeChannel' },
+        { title: '销售大区', field: 'regionCode' },
+        { title: '销售网点', field: 'officeCode' },
+        { title: '开票方式', field: 'invoiceType' },
+        { title: '付款方式', field: 'paymentMethod' },
+        { title: '付款条件', field: 'paymentTerm' },
+        { title: '随货开票', field: 'invoiceByGoods' },
+        { title: 'SAP销售订单号', field: 'sapOrderCode' },
+        { title: 'SAP交货单号', field: 'sapDeliveryCode' },
+        { title: '交货方式', field: 'deliveryType' },
+        { title: '仓库', field: 'storageCode' },
+        { title: '客户订单号', field: 'customerPocode' },
+        { title: '是否有明细', field: 'isDetails' },
+        { title: '收样网点', field: 'sampleOfficeCode' },
+        { title: '送货地址', field: 'address' },
+        { title: '作废人姓名', field: 'cancelName' },
+        { title: '作废时间', field: 'cancelDate' },
+        { title: '完成时间', field: 'finishDate' },
+        { title: '完成人姓名', field: 'finishName' }
+      ];
+
+      columns.forEach(function (e) {
+        if (!e.width) e.width = 100;
+      });
+
+      this[tableName].columns = columns;
+      this[tableName].xTable = this.$refs[tableName].$refs.xTable;
+    },
+    // 查询
+    handleSearch () {
+      const tableName = 'orderTable';
+      this[tableName].loading = true;
+      const { currentPage, pageSize } = this[tableName].pagerConfig;
+
+      const queryParam = this.form.getFieldsValue();
+      const params = Object.assign({ page: currentPage, rows: pageSize }, queryParam);
+
+      this.$api.seqorder.getOrderList(params, true).then((data) => {
+        this[tableName].tableData = data.rows;
+        this[tableName].pagerConfig.total = data.total;
+        this[tableName].pagerConfig.currentPage = params.page;
+        this[tableName].pagerConfig.pageSize = params.rows;
+      }).finally(() => {
+        this[tableName].loading = false;
+      });
     }
   }
 };
--- src/views/seq/sample.vue
+++ src/views/seq/sample.vue
@@ -3,33 +3,35 @@
   <div class="page-content">
 
     <div class="table-search">
-      <a-form layout="inline" :form="form" @submit="handleSearch">
+      <a-form layout="inline" :form="form" @submit.prevent="handleSearch">
         <a-row :gutter="24">
           <a-col :md="6" :xl="4">
+            <a-form-item label="编号">
+              <a-input v-decorator="['code']"/>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :xl="4">
+            <a-form-item label="名称">
+              <a-input v-decorator="['name']"/>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :xl="4">
+            <a-form-item label="别名">
+              <a-input v-decorator="['alias']"/>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :xl="4">
+            <a-form-item label="系列">
+              <a-select v-decorator="['seriesId', {initialValue: ''}]">
+                <a-select-option value="">全部</a-select-option>
+                <a-select-option v-for="series in $store.state.seq.series" :value="series.id" :key="series.id">{{ series.name }}</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :xl="4">
             <a-form-item label="状态">
-              <a-select v-decorator="['status']">
+              <a-select v-decorator="['status', {initialValue: 1}]">
                 <a-select-option value="">全部</a-select-option>
-                <a-select-option v-for="status in $store.state.basic.status" :value="status.id" :key="status.id">{{ status.name }}</a-select-option>
-              </a-select>
-            </a-form-item>
-          </a-col>
-          <a-col :md="6" :xl="4">
-            <a-form-item label="样品类型">
-              <a-select v-decorator="['sampleTypeId']">
-                <a-select-option value="">全部</a-select-option>
-                <a-select-option v-for="status in $store.state.basic.status" :value="status.id" :key="status.id">{{ status.name }}</a-select-option>
-              </a-select>
-            </a-form-item>
-          </a-col>
-          <a-col :md="6" :xl="4">
-            <a-form-item label="样品用量">
-              <a-input v-decorator="['sampleDose']"/>
-            </a-form-item>
-          </a-col>
-          <a-col :md="6" :xl="4">
-            <a-form-item label="测序点">
-              <a-select v-decorator="['seqfactoryIdList']">
-                <a-select-option value="0">全部</a-select-option>
                 <a-select-option v-for="status in $store.state.basic.status" :value="status.id" :key="status.id">{{ status.name }}</a-select-option>
               </a-select>
             </a-form-item>
@@ -42,102 +44,113 @@
     <div class="table-operator">
       <a-button-group>
         <a-button icon="search" @click="handleSearch">查询</a-button>
-        <a-button icon="plus">新建</a-button>
-        <a-button icon="form">修改</a-button>
-        <a-button icon="delete">删除</a-button>
-        <a-button icon="save">保存</a-button>
+        <a-button icon="plus" type="primary">新建</a-button>
       </a-button-group>
     </div>
 
-    <s-table
-      ref="table"
-      bordered
-      size="small"
-      :scroll="{ x: 2500 }"
-      :columns="columns"
-      :data="loadData"
-      :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
-    >
-    </s-table>
+    <vxe-grid
+      highlight-hover-row
+      auto-resize
+      height="600"
+      ref="sampleTable"
+      :loading="sampleTable.loading"
+      :columns="sampleTable.columns"
+      :pager-config="sampleTable.pagerConfig"
+      :data.sync="sampleTable.tableData"
+      @page-change="({pageSize, currentPage}) => this.$utils.tablePageChange({pageSize, currentPage, table: sampleTable, callback: handleSearch})">
+    </vxe-grid>
   </div>
 </template>
 
 <script>
-import STable from '@/components/Table';
-
 export default {
-  name: 'SeqSampleOrder',
+  name: 'SeqSample',
   components: {
-    STable
   },
   data () {
     return {
       form: this.$form.createForm(this),
-      advanced: true,
-      columns: [],
       queryParam: {},
-      loadData: parameter => {
-        const params = Object.assign(parameter, this.queryParam);
-        return this.$api.sample.getSample(params, true).then(res => {
-          return {
-            data: res.rows,
-            page: params.page,
-            total: res.total
-          };
-        });
-      },
-      selectedRowKeys: [],
-      selectedRows: []
+      sampleTable: {
+        id: 0,
+        xTable: null,
+        loading: false,
+        tableData: [],
+        columns: [],
+        pagerConfig: {
+          currentPage: 1,
+          pageSize: 10,
+          total: 0
+        }
+      }
     };
   },
   mounted () {
-    this.createColumnDefs();
+    this.setColumn();
+    this.handleSearch();
   },
   methods: {
-    createColumnDefs () {
-      const { formatter } = this.$utils;
-      const { basic } = this.$store.state;
+    // 设置表格列属性
+    setColumn () {
+      const tableName = 'sampleTable';
+      // const { formatter } = this.$utils;
+      // const { basic, seq } = this.$store.state;
 
-      this.columns = [
-        { title: '编号', dataIndex: 'code' },
-        { title: '名称', dataIndex: 'name' },
-        { title: '类型', dataIndex: 'sampleTypeId' },
-        { title: '过期时间', dataIndex: 'overdueDate' },
-        { title: '载体', dataIndex: 'carrierName' },
-        { title: '抗性', dataIndex: 'sampleResistanceName' },
-        { title: '特性', dataIndex: 'sampleFeatureName' },
-        { title: '最小长度', dataIndex: 'minLength' },
-        { title: '最大长度', dataIndex: 'maxLength' },
-        { title: '状态', dataIndex: 'status', customRender: function (text, record, index) { return formatter(basic.status, text); } },
-        { title: '失效原因', dataIndex: 'expireReason' },
-        { title: '客户', dataIndex: 'customerName' },
-        { title: '负责人', dataIndex: 'subcustomerName' },
-        { title: '订货人', dataIndex: 'contactName' },
-        { title: '销售员', dataIndex: 'salerName' },
-        { title: '网点', dataIndex: 'officeCode' },
-        { title: '大区', dataIndex: 'regionCode' },
-        { title: '测序点', dataIndex: 'seqfactoryName' },
-        { title: '收样人', dataIndex: 'samplerName' },
-        { title: '收样时间', dataIndex: 'samplerDate' },
-        { title: '创建人', dataIndex: 'creatorName' },
-        { title: '创建时间', dataIndex: 'createDate' },
-        { title: '失效人', dataIndex: 'expireName' },
-        { title: '失效时间', dataIndex: 'expireDate' },
-        { title: '作废人', dataIndex: 'cancelName' },
-        { title: '作废时间', dataIndex: 'cancelDate' }
+      const columns = [
+        { type: 'radio', width: 40 },
+        { type: 'index', width: 40 },
+        { title: '编号', field: 'code' },
+        { title: '名称', field: 'name' },
+        { title: '类型', field: 'sampleTypeId' },
+        { title: '过期时间', field: 'overdueDate' },
+        { title: '载体', field: 'carrierName' },
+        { title: '抗性', field: 'sampleResistanceName' },
+        { title: '特性', field: 'sampleFeatureName' },
+        { title: '最小长度', field: 'minLength' },
+        { title: '最大长度', field: 'maxLength' },
+        { title: '状态', field: 'status' },
+        { title: '失效原因', field: 'expireReason' },
+        { title: '客户', field: 'customerName' },
+        { title: '负责人', field: 'subcustomerName' },
+        { title: '订货人', field: 'contactName' },
+        { title: '销售员', field: 'salerName' },
+        { title: '网点', field: 'officeCode' },
+        { title: '大区', field: 'regionCode' },
+        { title: '测序点', field: 'seqfactoryName' },
+        { title: '收样人', field: 'samplerName' },
+        { title: '收样时间', field: 'samplerDate' },
+        { title: '创建人', field: 'creatorName' },
+        { title: '创建时间', field: 'createDate' },
+        { title: '失效人', field: 'expireName' },
+        { title: '失效时间', field: 'expireDate' },
+        { title: '作废人', field: 'cancelName' },
+        { title: '作废时间', field: 'cancelDate' }
       ];
+
+      columns.forEach(function (e) {
+        if (!e.width) e.width = 100;
+      });
+
+      this[tableName].columns = columns;
+      this[tableName].xTable = this.$refs[tableName].$refs.xTable;
     },
-    handleSearch (e) {
-      e.preventDefault();
-      this.queryParam = this.form.getFieldsValue();
-      this.$refs.table.refresh(true);
-    },
-    onSelectChange (selectedRowKeys, selectedRows) {
-      this.selectedRowKeys = selectedRowKeys;
-      this.selectedRows = selectedRows;
-    },
-    toggleAdvanced () {
-      this.advanced = !this.advanced;
+    // 查询
+    handleSearch () {
+      const tableName = 'sampleTable';
+      this[tableName].loading = true;
+      const { currentPage, pageSize } = this[tableName].pagerConfig;
+
+      const queryParam = this.form.getFieldsValue();
+      const params = Object.assign({ page: currentPage, rows: pageSize }, queryParam);
+
+      this.$api.sample.getSample(params, true).then((data) => {
+        this[tableName].tableData = data.rows;
+        this[tableName].pagerConfig.total = data.total;
+        this[tableName].pagerConfig.currentPage = params.page;
+        this[tableName].pagerConfig.pageSize = params.rows;
+      }).finally(() => {
+        this[tableName].loading = false;
+      });
     }
   }
 };
