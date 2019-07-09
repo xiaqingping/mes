e278f2da7683094e6ad6c80dde2e004f44f11603
Meng YuCheng <maxmeng93@gmail.com>
2019-07-09 14:56:21 +0800

    修改webpack配置打包优化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack'); // 引入webpack库
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
@@ -5,14 +6,33 @@ module.exports = {
   // 部署应用的基本URL
   publicPath: '/',
   // webpack 配置
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'vue$': path.resolve(__dirname, 'node_modules/vue/dist/vue.js')
+  configureWebpack: config => {
+    let meConfig = {
+      resolve: {
+        alias: {
+          'vue$': path.resolve(__dirname, 'node_modules/vue/dist/vue.js')
+        }
       }
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+      // 生产
+      meConfig = Object.assign(meConfig, {
+        optimization: {
+          splitChunks: {
+            maxSize: 1000000
+          }
+        }
+      });
+    } else {
+      // 开发
     }
+
+    return meConfig;
   },
   chainWebpack: config => {
+    config.plugin('ignore')
+      .use(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)); // 忽略/moment/locale下的所有文件
     if (process.env.use_analyzer) {
       config.plugin('webpack-bundle-analyzer').use(BundleAnalyzerPlugin);
     }
