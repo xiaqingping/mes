c5863649a87012ae4506fa45c66eeaa7cb6cc152
Meng YuCheng <maxmeng93@gmail.com>
2019-09-09 16:25:14 +0800

    update

--- src/components/CustomizedFormControls/index.jsx
+++ src/components/CustomizedFormControls/index.jsx
@@ -58,67 +58,102 @@ const options = [
 ];
 
 // 名称
-export function NameInput() {
-  return (
-    <InputGroup compact>
-      <Select defaultValue="1" style={{ width: '40%' }}>
-        <Option value="1"><Icon type="user" /> 个人</Option>
-        <Option value="2"><Icon type="home" /> 组织</Option>
-      </Select>
-      <Input style={{ width: '60%' }} />
-    </InputGroup>
-  );
+export class NameInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <InputGroup compact>
+        <Select defaultValue="1" style={{ width: '40%' }}>
+          <Option value="1"><Icon type="user" /> 个人</Option>
+          <Option value="2"><Icon type="home" /> 组织</Option>
+        </Select>
+        <Input style={{ width: '60%' }} />
+      </InputGroup>
+    );
+  }
 }
 
 // 移动电话
-export function MobileTelephoneInput() {
-  return (
-    <InputGroup compact>
-      <Select defaultValue="+86" style={{ width: '40%' }}>
-        <Option value="+86">+86</Option>
-        <Option value="+01">+01</Option>
-      </Select>
-      <Input style={{ width: '60%' }} />
-    </InputGroup>
-  );
+export class MobileTelephoneInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <InputGroup compact>
+        <Select defaultValue="+86" style={{ width: '40%' }}>
+          <Option value="+86">+86</Option>
+          <Option value="+01">+01</Option>
+        </Select>
+        <Input style={{ width: '60%' }} />
+      </InputGroup>
+    );
+  }
 }
 
 // 电话
-export function TelphoneInput() {
-  return (
-    <InputGroup compact>
-      <Select defaultValue="+86" style={{ width: '30%' }}>
-        <Option value="+86">+86</Option>
-        <Option value="+01">+01</Option>
-      </Select>
-      <Input style={{ width: '20%' }} />
-      <Input style={{ width: '30%' }} />
-      <Input style={{ width: '20%' }} />
-    </InputGroup>
-  );
+export class TelphoneInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <InputGroup compact>
+        <Select defaultValue="+86" style={{ width: '30%' }}>
+          <Option value="+86">+86</Option>
+          <Option value="+01">+01</Option>
+        </Select>
+        <Input style={{ width: '20%' }} />
+        <Input style={{ width: '30%' }} />
+        <Input style={{ width: '20%' }} />
+      </InputGroup>
+    );
+  }
 }
 
 // 传真
-export function FoxInput() {
-  return (
-    <InputGroup compact>
-      <Select defaultValue="+86" style={{ width: '30%' }}>
-        <Option value="+86">+86</Option>
-        <Option value="+01">+01</Option>
-      </Select>
-      <Input style={{ width: '20%' }} />
-      <Input style={{ width: '30%' }} />
-      <Input style={{ width: '20%' }} />
-    </InputGroup>
-  );
+export class FoxInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <InputGroup compact>
+        <Select defaultValue="+86" style={{ width: '30%' }}>
+          <Option value="+86">+86</Option>
+          <Option value="+01">+01</Option>
+        </Select>
+        <Input style={{ width: '20%' }} />
+        <Input style={{ width: '30%' }} />
+        <Input style={{ width: '20%' }} />
+      </InputGroup>
+    );
+  }
 }
 
 // 地址
-export function AddressInput() {
-  return (
-    <InputGroup compact>
-      <Cascader options={options} style={{ width: '40%' }} />
-      <Input style={{ width: '60%' }} />
-    </InputGroup>
-  );
+export class AddressInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <InputGroup compact>
+        <Cascader options={options} style={{ width: '40%' }} />
+        <Input style={{ width: '60%' }} />
+      </InputGroup>
+    );
+  }
 }
