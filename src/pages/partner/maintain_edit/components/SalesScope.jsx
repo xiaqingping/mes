4a528fa44566ac639d404c987636c452497f1ac2
Meng YuCheng <maxmeng93@gmail.com>
2019-09-18 15:36:44 +0800

    update

--- src/pages/partner/maintain_edit/components/SalesScope.jsx
+++ src/pages/partner/maintain_edit/components/SalesScope.jsx
@@ -7,6 +7,10 @@ import {
   Switch,
 } from 'antd';
 import React, { Component } from 'react';
+import BillToParty from './BillToParty';
+import SoldToParty from './SoldToParty';
+import ShipToParty from './ShipToParty';
+import Salesperson from './Salesperson';
 
 const FormItem = Form.Item;
 const { Option } = Select;
@@ -30,15 +34,21 @@ const tabListNoTitle = [
   },
 ];
 
+const list = {
+  BillToParty: <BillToParty />,
+  SoldToParty: <SoldToParty />,
+  ShipToParty: <ShipToParty />,
+  Salesperson: <Salesperson />,
+};
+
 class BasicInfo extends Component {
   state = {
-    noTitleKey: '1',
+    key1: '1',
+    key2: 'BillToParty',
   }
 
-  onTabChange = key => {
-    this.setState({
-      noTitleKey: key,
-    });
+  onTabChange = obj => {
+    this.setState(obj);
   }
 
   render() {
@@ -51,9 +61,9 @@ class BasicInfo extends Component {
         bordered={false}
         style={{ width: '100%', marginBottom: '24px' }}
         tabList={tabListNoTitle}
-        activeTabKey={this.state.noTitleKey}
+        activeTabKey={this.state.key1}
         onTabChange={key => {
-          this.onTabChange(key);
+          this.onTabChange({ key1: key });
         }}
       >
         <Form>
@@ -106,6 +116,20 @@ class BasicInfo extends Component {
             </Col>
           </Row>
         </Form>
+        <Card
+          tabList={[
+            { key: 'BillToParty', tab: '开票方' },
+            { key: 'SoldToParty', tab: '售达方' },
+            { key: 'ShipToParty', tab: '送达方' },
+            { key: 'Salesperson', tab: '销售员' },
+          ]}
+          activeTabKey={this.state.key2}
+          onTabChange={key => {
+            this.onTabChange({ key2: key });
+          }}
+        >
+          {list[this.state.key2]}
+        </Card>
       </Card>
     );
   }
