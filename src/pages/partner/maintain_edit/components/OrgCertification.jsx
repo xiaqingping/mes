9bd4b5bf186d1b443d209ff6d23d35e719adaed1
Meng YuCheng <maxmeng93@gmail.com>
2019-10-16 10:13:56 +0800

    update

--- src/pages/partner/maintain_edit/components/OrgCertification.jsx
+++ src/pages/partner/maintain_edit/components/OrgCertification.jsx
@@ -18,8 +18,9 @@ const { TextArea } = Input;
 
 @connect(({ partnerMaintainEdit }) => ({
   details: partnerMaintainEdit.details || {},
-  // eslint-disable-next-line max-len
-  organizationCertification: (partnerMaintainEdit.details && partnerMaintainEdit.details.organizationCertification) || { attachmentList: [] },
+  organizationCertification: (
+    partnerMaintainEdit.details && partnerMaintainEdit.details.organizationCertification
+  ) || { attachmentList: [] },
 }))
 class OrgCertification extends Component {
   constructor(props) {
@@ -29,7 +30,6 @@ class OrgCertification extends Component {
 
   valueChange = (key, value) => {
     const { details, organizationCertification } = this.props;
-    // const { organizationCertification } = details;
     if (key === 'attachmentList') {
       value = value.fileList.map(e => ({
         code: 'https://blog.maxmeng.top/images/avatar.jpg',
@@ -52,10 +52,23 @@ class OrgCertification extends Component {
     });
   }
 
-  render() {
+  uploadButton = () => (
+    <div>
+      <Icon type="plus" />
+      <div className="ant-upload-text">Upload</div>
+    </div>
+  )
+
+  normFile = e => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  }
+
+  renderChina = () => {
     const {
       form: { getFieldDecorator },
-      // details: { organizationCertification: orgData },
       organizationCertification: orgData,
     } = this.props;
 
@@ -65,116 +78,187 @@ class OrgCertification extends Component {
       url: e.code,
     }));
 
-    const uploadButton = (
-      <div>
-        <Icon type="plus" />
-        <div className="ant-upload-text">Upload</div>
-      </div>
+    return (
+      <Form>
+        <Row gutter={32}>
+          <Col xxl={15} lg={24}>
+            <Row gutter={32}>
+              <Col span={8}>
+                <FormItem label="认证状态">
+                  <Badge status="default" />未认证
+                </FormItem>
+              </Col>
+              <Col span={8}>
+                <FormItem label="增值税专用发票资质">
+                  {getFieldDecorator('specialInvoice', {
+                    valuePropName: 'checked',
+                    initialValue: orgData.specialInvoice,
+                  })(<Switch onChange={value => this.valueChange('specialInvoice', value)} />)}
+                </FormItem>
+              </Col>
+              <Col span={8}>
+                <FormItem label="统一社会信用代码">
+                  {getFieldDecorator('taxNo', {
+                    initialValue: orgData.taxNo,
+                  })(<Input onChange={e => this.valueChange('taxNo', e.target.value)} />)}
+                </FormItem>
+              </Col>
+              <Col span={8}>
+                <FormItem label="基本户开户银行">
+                  {getFieldDecorator('bankCode', {
+                    initialValue: orgData.bankCode,
+                  })(<Input onChange={e => this.valueChange('bankCode', e.target.value)} />)}
+                </FormItem>
+              </Col>
+              <Col span={8}>
+                <FormItem label="基本户开户账号">
+                  {getFieldDecorator('bankAccount', {
+                    initialValue: orgData.bankAccount,
+                  })(<Input onChange={e => this.valueChange('bankAccount', e.target.value)} />)}
+                </FormItem>
+              </Col>
+              <Col span={8}>
+                <FormItem label="电话">
+                  {getFieldDecorator('telephone', {
+                    initialValue: {
+                      telephoneCountryCode: orgData.telephoneCountryCode,
+                      telephoneAreaCode: orgData.telephoneAreaCode,
+                      telephone: orgData.telephone,
+                      telephoneExtension: orgData.telephoneExtension,
+                    },
+                  })(<TelphoneInput onChange={value => this.valueChange('telephone', value)} />)}
+                </FormItem>
+              </Col>
+              <Col span={24}>
+                <FormItem label="注册地址">
+                  {getFieldDecorator('address', {
+                    initialValue: orgData.address,
+                  })(<Input onChange={e => this.valueChange('address', e.target.value)} />)}
+                </FormItem>
+              </Col>
+            </Row>
+          </Col>
+          <Col xxl={9} lg={24}>
+            <FormItem label="认证说明">
+              {getFieldDecorator('notes', {
+                initialValue: orgData.notes,
+              })(<TextArea rows={11} onChange={e => this.valueChange('notes', e.target.value)} />)}
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <FormItem label="认证图片">
+              {getFieldDecorator('attachmentList', {
+                rules: [{ required: true }],
+                valuePropName: 'attachmentList',
+                getValueFromEvent: this.normFile,
+              })(<Upload
+                name="file"
+                listType="picture-card"
+                showUploadList
+                action="/upload"
+                accept=".jpg,.png"
+                fileList={fileList}
+                onChange={value => this.valueChange('attachmentList', value)}
+              >
+                {this.uploadButton()}
+              </Upload>)}
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
     );
+  }
 
-    const normFile = e => {
-      if (Array.isArray(e)) {
-        return e;
-      }
-      return e && e.fileList;
-    };
+  renderOther = () => {
+    const {
+      form: { getFieldDecorator },
+      organizationCertification: orgData,
+    } = this.props;
 
+    const fileList = orgData.attachmentList && orgData.attachmentList.map(e => ({
+      uid: e.code,
+      name: e.name,
+      url: e.code,
+    }));
+
+    return (
+      <Form>
+        <Row gutter={64}>
+          <Col span={6}>
+            <Row>
+              <Col span={24}>
+                <FormItem label="认证状态">
+                  <Badge status="default" />未认证
+                </FormItem>
+              </Col>
+              <Col span={24}>
+                <FormItem label="增值税登记号">
+                  {getFieldDecorator('taxNo', {
+                    initialValue: orgData.taxNo,
+                  })(<Input onChange={e => this.valueChange('taxNo', e.target.value)} />)}
+                </FormItem>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={6}>
+            <FormItem label="认证图片">
+              {getFieldDecorator('attachmentList', {
+                rules: [{ required: true }],
+                valuePropName: 'attachmentList',
+                getValueFromEvent: this.normFile,
+              })(<Upload
+                name="file"
+                listType="picture-card"
+                showUploadList
+                action="/upload"
+                accept=".jpg,.png"
+                fileList={fileList}
+                onChange={value => this.valueChange('attachmentList', value)}
+              >
+                {this.uploadButton()}
+              </Upload>)}
+            </FormItem>
+          </Col>
+          <Col span={10}>
+            <FormItem label="认证说明">
+              {getFieldDecorator('notes', {
+                initialValue: orgData.notes,
+              })(<TextArea rows={6} onChange={e => this.valueChange('notes', e.target.value)} />)}
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+
+  renderForm = () => {
+    const { details: { basic } } = this.props;
+    const { countryCode } = basic;
+    let form;
+    switch (countryCode) {
+      case 'china':
+        form = this.renderChina;
+        break;
+      case 'USA':
+        form = this.renderOther;
+        break;
+      default:
+        form = this.renderChina;
+        break;
+    }
+    return form();
+  }
+
+  render() {
     return (
       <Card
         title="认证资料"
         bordered={false}
         style={{ marginBottom: '24px' }}
       >
-        <Form>
-          <Row gutter={32}>
-            <Col xxl={15} lg={24}>
-              <Row gutter={32}>
-                <Col span={8}>
-                  <FormItem label="认证状态">
-                    <Badge status="success" />已认证
-                    <a style={{ marginLeft: '1em' }}>取消认证</a>
-                  </FormItem>
-                </Col>
-                <Col span={8}>
-                  <FormItem label="增值税专用发票资质">
-                    {getFieldDecorator('specialInvoice', {
-                      valuePropName: 'checked',
-                      initialValue: orgData.specialInvoice,
-                    })(<Switch onChange={value => this.valueChange('specialInvoice', value)} />)}
-                  </FormItem>
-                </Col>
-                <Col span={8}>
-                  <FormItem label="统一社会信用代码">
-                    {getFieldDecorator('taxNo', {
-                      initialValue: orgData.taxNo,
-                    })(<Input onChange={e => this.valueChange('taxNo', e.target.value)} />)}
-                  </FormItem>
-                </Col>
-                <Col span={8}>
-                  <FormItem label="基本户开户银行">
-                    {getFieldDecorator('bankCode', {
-                      initialValue: orgData.bankCode,
-                    })(<Input onChange={e => this.valueChange('bankCode', e.target.value)} />)}
-                  </FormItem>
-                </Col>
-                <Col span={8}>
-                  <FormItem label="基本户开户账号">
-                    {getFieldDecorator('bankAccount', {
-                      initialValue: orgData.bankAccount,
-                    })(<Input onChange={e => this.valueChange('bankAccount', e.target.value)} />)}
-                  </FormItem>
-                </Col>
-                <Col span={8}>
-                  <FormItem label="电话">
-                    {getFieldDecorator('telephone', {
-                      initialValue: {
-                        telephoneCountryCode: orgData.telephoneCountryCode,
-                        telephoneAreaCode: orgData.telephoneAreaCode,
-                        telephone: orgData.telephone,
-                        telephoneExtension: orgData.telephoneExtension,
-                      },
-                    })(<TelphoneInput onChange={value => this.valueChange('telephone', value)} />)}
-                  </FormItem>
-                </Col>
-                <Col span={24}>
-                  <FormItem label="注册地址">
-                    {getFieldDecorator('address', {
-                      initialValue: orgData.address,
-                    })(<Input onChange={e => this.valueChange('address', e.target.value)} />)}
-                  </FormItem>
-                </Col>
-              </Row>
-            </Col>
-            <Col xxl={9} lg={24}>
-              <FormItem label="认证说明">
-                {getFieldDecorator('notes', {
-                  initialValue: orgData.notes,
-                })(<TextArea rows={11} onChange={e => this.valueChange('notes', e.target.value)} />)}
-              </FormItem>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <FormItem label="认证图片">
-                {getFieldDecorator('attachmentList', {
-                  rules: [{ required: true }],
-                  valuePropName: 'attachmentList',
-                  getValueFromEvent: normFile,
-                })(<Upload
-                  name="file"
-                  listType="picture-card"
-                  showUploadList
-                  action="/upload"
-                  accept=".jpg,.png"
-                  fileList={fileList}
-                  onChange={value => this.valueChange('attachmentList', value)}
-                >
-                  {uploadButton}
-                </Upload>)}
-              </FormItem>
-            </Col>
-          </Row>
-        </Form>
+        {this.renderForm()}
       </Card>
     );
   }
