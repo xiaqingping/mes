536c61274ab4607c1d00bc3a5a44118dd7bcd4de
maxmeng <maxmeng93@gmail.com>
2020-02-27 15:34:10 +0800

    update

--- config/config.js
+++ config/config.js
@@ -2,15 +2,12 @@ import defaultSettings from './defaultSettings'; // https://umijs.org/config/
 
 import slash from 'slash2';
 import webpackPlugin from './plugin.config';
-import themePluginConfig from './themePluginConfig';
 import proxy from './proxy';
-const { pwa } = defaultSettings; // preview.pro.ant.design only do not use in your production ;
-// preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
+const { pwa } = defaultSettings;
 
 // 所有路由信息
 const routes = require('./router');
-const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION, REACT_APP_ENV, BASE_API } = process.env;
-const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
+const { REACT_APP_ENV, BASE_API } = process.env;
 const plugins = [
   ['umi-plugin-antd-icon-config', {}],
   [
@@ -48,27 +45,7 @@ const plugins = [
       // },
     },
   ],
-  [
-    'umi-plugin-pro-block',
-    {
-      moveMock: false,
-      moveService: false,
-      modifyRequest: true,
-      autoAddMenu: true,
-    },
-  ],
 ];
-
-if (isAntDesignProPreview) {
-  // 针对 preview.pro.ant.design 的 GA 统计代码
-  plugins.push([
-    'umi-plugin-ga',
-    {
-      code: 'UA-72788897-6',
-    },
-  ]);
-  plugins.push(['umi-plugin-antd-theme', themePluginConfig]);
-}
 
 export default {
   plugins,
@@ -85,8 +62,6 @@ export default {
   define: {
     BASE_API: BASE_API || 'dev',
     REACT_APP_ENV: REACT_APP_ENV || false,
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION:
-      ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION || '', // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
   },
   ignoreMomentLocale: true,
   lessLoaderOptions: {
@@ -122,17 +97,5 @@ export default {
     basePath: '/',
   },
   // chainWebpack: webpackPlugin,
-  // proxy: proxy[REACT_APP_ENV || 'dev'],
-  proxy: {
-    '/192.168.19.71:8550/': {
-      target: 'http://192.168.19.71:8550/',
-      changeOrigin: true,
-      pathRewrite: { '^/192.168.19.71:8550/': '' },
-    },
-    '/192.168.19.71:8001/': {
-      target: 'http://192.168.19.71:8001/',
-      changeOrigin: true,
-      pathRewrite: { '^/192.168.19.71:8001/': '' },
-    },
-  },
+  proxy,
 };
