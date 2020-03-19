81509cf481af9cb8f7c619cee8bf2ea519cd1462
niko <232641655@qq.com>
2020-03-19 10:11:10 +0800

    update

--- src/pages/project-manage/locales/en-US.js
+++ src/pages/project-manage/locales/en-US.js
@@ -1,9 +1,5 @@
+import menu from './en-US/menu';
+
 export default {
-  'menu.project': 'Project Manage',
-  'menu.project.project-manage': 'Project Manage',
-  'menu.project.task-manage': 'Task Manage',
-  'menu.project.project-model': 'Project Model',
-  'menu.project.task-model': 'Task Model',
-  'menu.project.project-model-type': 'Project Model Type',
-  'menu.project.task-model-type': 'Task Model Type',
+  ...menu,
 };
