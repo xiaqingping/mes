b142d811058098b768fb321474654957c7d2806a
niko <232641655@qq.com>
2020-03-19 09:51:29 +0800

    update

--- src/locales/en-US/menu.js
+++ src/locales/en-US/menu.js
@@ -62,11 +62,4 @@ export default {
   'menu.purchase.supplier': 'Supplier Subjects',
   'menu.hts': '高通量',
   'menu.hts.home': '示例',
-  'menu.project': 'Project Manage',
-  'menu.project.project-managet': 'Project Manage',
-  'menu.project.task-manage': 'Task Manage',
-  'menu.project.project-model': 'Project Model',
-  'menu.project.task-model': 'Task Model',
-  'menu.project.project-model-type': 'Project Model Type',
-  'menu.project.task-model-type': 'Task Model Type',
 };
--- src/locales/zh-CN/menu.js
+++ src/locales/zh-CN/menu.js
@@ -62,11 +62,4 @@ export default {
   'menu.purchase.supplier': '供应商科目',
   'menu.hts': '高通量',
   'menu.hts.home': '示例',
-  'menu.project': '项目管理',
-  'menu.project.project-manage': '项目管理',
-  'menu.project.task-manage': '任务管理',
-  'menu.project.project-model': '项目模型',
-  'menu.project.task-model': '任务模型',
-  'menu.project.project-model-type': '项目模型类别',
-  'menu.project.task-model-type': '任务模型类别',
 };
