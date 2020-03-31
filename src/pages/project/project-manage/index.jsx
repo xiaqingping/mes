0c957399864f8328b09b479b7b740480874cffb6
wuhezhen <wuhezhen@sangon.com>
2020-03-31 11:22:54 +0800

    333

--- src/pages/project/project-manage/index.jsx
+++ src/pages/project/project-manage/index.jsx
@@ -16,6 +16,7 @@ import router from 'umi/router';
 import StandardTable from '../components/StandardTable';
 // import api from '@/api';
 import { InputUI, SelectUI, DateUI } from '../components/AntdSearchUI';
+// import { DrawerTool } from '../components/AntdUI';
 // import { expandedRowRender } from '../functions';
 
 class ProjectManagement extends Component {
@@ -39,29 +40,6 @@ class ProjectManagement extends Component {
 
   // 获取表格数据
   getTableData = (options = {}) => {
-    // this.setState({ loading: true });
-    // const formData = this.tableSearchFormRef.current.getFieldsValue();
-    // const { pagination } = this.state;
-    // const { current: page, pageSize: rows } = pagination;
-    // const data = {
-    //   page,
-    //   rows,
-    //   ...formData,
-    //   ...options,
-    // };
-
-    // api.peptideBase.getPurity(data, true).then(res => {
-    //   this.setState({
-    //     list: res.rows,
-    //     pagination: {
-    //       current: data.page,
-    //       pageSize: data.rows,
-    //       total: res.total,
-    //     },
-    //     loading: false,
-    //     editIndex: -1,
-    //   });
-    // });
     const data = this.props.project.projectManage;
     this.setState({
       list: data,
@@ -97,6 +75,8 @@ class ProjectManagement extends Component {
           languageCode={languageCode}
           label="状态"
           name="status"
+          mode="multiple"
+          allowClear
           data={[
             { value: 1, data: '未开始', key: '1' },
             { value: 2, data: '进行中', key: '2' },
@@ -129,6 +109,11 @@ class ProjectManagement extends Component {
       {
         title: '编号/名称',
         dataIndex: 'code',
+        render: value => (
+          <>
+            <a onClick={() => this.searchDetails(value)}>{value}</a>
+          </>
+        ),
       },
       {
         title: '描述',
@@ -223,85 +208,6 @@ class ProjectManagement extends Component {
       },
     ];
 
-    // 子table
-    // let sonTablecolumns = [
-    //   {
-    //     title: '编号/名称',
-    //     dataIndex: 'code',
-    //   },
-    //   {
-    //     title: '描述',
-    //     dataIndex: 'describe',
-    //   },
-    //   {
-    //     title: '创建人/时间',
-    //     dataIndex: 'creatorName',
-    //     render: (value, row) => (
-    //       <>
-    //         {value}
-    //         <br />
-    //         {row.creatorTime}
-    //       </>
-    //     ),
-    //   },
-    //   {
-    //     title: '修改人/时间',
-    //     dataIndex: 'changerName',
-    //     render: (value, row) => (
-    //       <>
-    //         {value}
-    //         <br />
-    //         {row.changerTime}
-    //       </>
-    //     ),
-    //   },
-    //   {
-    //     title: '状态',
-    //     dataIndex: 'status',
-    //     width: '80px',
-    //     render: value =>
-    //       value === 1 ? <Badge color="green" text="有效" /> : <Badge color="red" text="无效" />,
-    //   },
-    //   {
-    //     title: '成员数',
-    //     dataIndex: 'memberNumber',
-    //   },
-    //   {
-    //     title: '项目模型名称/版本',
-    //     dataIndex: 'projectModelName',
-    //     render: (value, row) => (
-    //       <>
-    //         {value}
-    //         <br />
-    //         <Tag color="success">&nbsp;&nbsp;V{row.copyRight}&nbsp;&nbsp;</Tag>
-    //       </>
-    //     ),
-    //   },
-    //   {
-    //     title: '开始-截止时间',
-    //     dataIndex: 'StartTime',
-    //     render: (value, row) => (
-    //       <>
-    //         {value}
-    //         <br />
-    //         {row.endTime}
-    //       </>
-    //     ),
-    //   },
-    //   {
-    //     title: '操作',
-    //     render: () => (
-    //       <>
-    //         <a onClick={() => console.log(111)}>完成</a>
-    //         <Divider type="vertical" />
-    //         <a onClick={() => console.log(222)}>关闭</a>
-    //         <Divider type="vertical" />
-    //         <a onClick={() => console.log(333)}>挂起</a>
-    //       </>
-    //     ),
-    //   },
-    // ];
-
     columns = columns.map(col => {
       // eslint-disable-next-line no-param-reassign
       if (!col.width) col.width = 100;
@@ -311,16 +217,6 @@ class ProjectManagement extends Component {
       }
       return true;
     });
-
-    // sonTablecolumns = sonTablecolumns.map(col => {
-    //   // eslint-disable-next-line no-param-reassign
-    //   if (!col.width) col.width = 100;
-    //   tableWidth += col.width;
-    //   if (!col.editable) {
-    //     return col;
-    //   }
-    //   return true;
-    // });
 
     return (
       <PageHeaderWrapper>
@@ -348,11 +244,6 @@ class ProjectManagement extends Component {
                 columns={columns}
                 onSelectRow={this.handleSelectRows}
                 onChange={this.handleStandardTableChange}
-                // expandable={{
-                //   // 用方法创建子table
-                //   expandedRowRender: value => expandedRowRender(value.list, sonTablecolumns),
-                //   rowExpandable: record => !!record.list,
-                // }}
               />
             </Form>
           </div>
