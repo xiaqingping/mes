749d64545a7da662914c3376bc8fcd849d073c4a
niko <232641655@qq.com>
2020-04-02 17:23:56 +0800

    update

--- src/layouts/SecurityLayout.jsx
+++ src/layouts/SecurityLayout.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
 import { connect } from 'dva';
+import { PageLoading } from '@ant-design/pro-layout';
 import { Redirect } from 'umi';
-import PageLoading from '@/components/PageLoading';
+import { basic } from '@/api';
+
+const mouses = {
+  x: undefined,
+  y: undefined,
+  acter: undefined,
+  timer: undefined,
+  inter: undefined,
+  active(e, call) {
+    clearTimeout(mouses.acter);
+    mouses.acter = setTimeout(() => {
+      mouses.x = undefined;
+      mouses.y = undefined;
+    }, 1000);
+    if (!mouses.x || !mouses.y) {
+      mouses.x = e.pageX;
+      mouses.y = e.pageY;
+    }
+    const l = Math.abs(e.pageX - mouses.x) + Math.abs(e.pageY - mouses.y);
+    if (e.type === 'click' || (e.type === 'mousemove' && l > 100)) {
+      mouses.execute(call);
+    }
+  },
+  execute(call) {
+    clearTimeout(mouses.timer);
+    mouses.timer = setTimeout(() => {
+      clearTimeout(mouses.timer);
+      clearInterval(mouses.inter);
+      mouses.inter = undefined;
+    }, 1000 * 60 * 5);
+    if (!mouses.inter) {
+      mouses.inter = setInterval(call, 1000 * 60 * 2);
+      call();
+    }
+  },
+};
 
 class SecurityLayout extends React.Component {
   state = {
@@ -20,10 +56,42 @@ class SecurityLayout extends React.Component {
         type: 'user/fetchCurrent',
       });
     }
+
+    window.addEventListener('click', this.getNotice);
+    window.addEventListener('mousemove', this.getNotice);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.getNotice);
+    window.removeEventListener('mousemove', this.getNotice);
+  }
+
+  getNotice = e => {
+    mouses.active(e, () => {
+      // TODO: 这里的接口换为消息通知接口
+      basic.getPlants();
+    });
+  };
 
   render() {
     const { isReady } = this.state;
+    // const { children, loading, currentUser } = this.props;
+    // // You can replace it to your authentication rule (such as check token exists)
+    // // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
+
+    // const isLogin = currentUser && currentUser.userid;
+    // const queryString = stringify({
+    //   redirect: window.location.href,
+    // });
+
+    // if ((!isLogin && loading) || !isReady) {
+    //   return <PageLoading />;
+    // }
+
+    // if (!isLogin) {
+    //   return <Redirect to={`/user/login?${queryString}`} />;
+    // }
+
     const { children, loading } = this.props;
     const authorization = localStorage.getItem('token');
 
@@ -32,7 +100,7 @@ class SecurityLayout extends React.Component {
     }
 
     if (!authorization) {
-      return <Redirect to="/user/login"></Redirect>;
+      return <Redirect to="/user/login" />;
     }
 
     return children;
