7685df820513045bc28a4cc3179f858b23289cf3
niko <232641655@qq.com>
2020-04-07 17:04:30 +0800

    update

--- src/pages/project/api/processModel.js
+++ src/pages/project/api/processModel.js
@@ -1,21 +1,21 @@
 import request from '@/utils/request';
 
 // 2组
-let http2 = 'http://192.168.20.12:8360';
+let http2 = 'http://192.168.20.12:8360/v1/process';
 if (process.env.NODE_ENV !== 'development') {
-  http2 = '/yue';
+  http2 = '/v1/process';
 }
 
 export default {
   // 流程模型接口
   // 流程模型分页列表
   getProcess(params) {
-    return request(`${http2}/v1/process`, { params });
+    return request(`${http2}`, { params });
   },
 
   // 创建流程模型
   addProcess(data) {
-    return request(`${http2}/v1/process`, {
+    return request(`${http2}`, {
       method: 'POST',
       data,
     });
@@ -23,7 +23,7 @@ export default {
 
   // 创建流程模型
   upgradeProcess(id, data) {
-    return request(`${http2}/v1/process/${id}/upgrade`, {
+    return request(`${http2}/${id}/upgrade`, {
       method: 'POST',
       data,
     });
@@ -31,7 +31,7 @@ export default {
 
   // 修改流程模型
   changeProcess(data) {
-    return request(`${http2}/v1/process`, {
+    return request(`${http2}`, {
       method: 'PUT',
       data,
     });
@@ -39,42 +39,42 @@ export default {
 
   // 删除流程模型
   deleteProcess(id) {
-    return request(`${http2}/v1/process/${id}/deletion`, {
+    return request(`${http2}/${id}/deletion`, {
       method: 'PUT',
     });
   },
 
   // 发布流程模型
   publishment(id) {
-    return request(`${http2}/v1/process/${id}/publishment`, {
+    return request(`${http2}/${id}/publishment`, {
       method: 'PUT',
     });
   },
 
   // 禁用流程模型
   unPublishment(id) {
-    return request(`${http2}/v1/process/${id}/forbiddance`, {
+    return request(`${http2}/${id}/forbiddance`, {
       method: 'PUT',
     });
   },
 
   // 查询任务模型详细信息
   getProcessDetail(id) {
-    return request(`${http2}/v1/process/${id}`);
+    return request(`${http2}/${id}`);
   },
 
   // 根据code和version查询流程模型详细信息
   getProcessChangeVersion(data) {
-    return request(`${http2}/v1/process/${data.code}/${data.version}`);
+    return request(`${http2}/${data.code}/${data.version}`);
   },
 
   // 流程模型编号模糊搜索
   getProcessCodeAndName(data) {
-    return request(`${http2}/v1/process/${data}/search`);
+    return request(`${http2}/${data}/search`);
   },
 
   // 流程模型发布人模糊搜索
   getProcessPublisherCodeAndName(data) {
-    return request(`${http2}/v1/process/publisher/${data}/search`);
+    return request(`${http2}/publisher/${data}/search`);
   },
 };
