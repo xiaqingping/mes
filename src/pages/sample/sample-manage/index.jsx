013fb72421d7f54a423617bfb136a73e1fe3a349
niko <232641655@qq.com>
2020-04-21 17:02:06 +0800

    update

--- src/pages/sample/sample-manage/index.jsx
+++ src/pages/sample/sample-manage/index.jsx
@@ -226,7 +226,6 @@ class ProcessModel extends Component {
 
   render() {
     const { pagination, loading, list, visible, detailVisible, detailValue } = this.state;
-    console.log(process.env);
     const columns = [
       {
         title: '编号/名称',
