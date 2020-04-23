174e3083ba104433b02b4148751c554eaa8376f8
YaoYinting <yaoyinting@sangon.com>
2020-04-23 21:02:20 +0800

    bug

--- src/pages/project/task-model/model.js
+++ src/pages/project/task-model/model.js
@@ -4,27 +4,24 @@ const SeqModel = {
   namespace: 'taskModel',
   state: {
     argumentList: null,
-    arguments: [
-      {
-        describe: 'string', // 描述
-        id: 'string',
-        paramKey: 'string', // name
-        paramProperties: [
-          // 其他选项列表
-          {
-            isRequired: true,
-          },
-          {
-            placeholder: '请输入',
-          },
-          {
-            defaultValue: '默认值',
-          },
-        ],
-      },
-    ],
-    formItemType: [
-      {
+    arguments: [{
+      describe: 'string', // 描述
+      id: 'string',
+      paramKey: 'string', // name
+      paramProperties: [
+        // 其他选项列表
+        {
+          isRequired: true,
+        },
+        {
+          placeholder: '请输入',
+        },
+        {
+          defaultValue: '默认值',
+        },
+      ],
+    }, ],
+    formItemType: [{
         type: 'input',
         text: '单行输入',
       },
@@ -48,8 +45,7 @@ const SeqModel = {
       //   text: "单选"
       // }
     ],
-    taskModelStatusOptions: [
-      {
+    taskModelStatusOptions: [{
         value: 1,
         data: '未发布',
         text: '未发布',
@@ -74,6 +70,38 @@ const SeqModel = {
         key: '3',
       },
     ],
+    modelStatusOptions: [{
+        value: 1,
+        data: '未发布',
+        text: '未发布',
+        key: '1',
+        label: '未发布',
+        status: 'default',
+      },
+      {
+        value: 2,
+        data: '已发布',
+        text: '已发布',
+        key: '2',
+        label: '已发布',
+        status: 'success',
+      },
+      {
+        label: '已禁用',
+        data: '已发布',
+        value: 3,
+        status: 'error',
+        text: '已禁用',
+        key: '3',
+      }, {
+        label: '已过期',
+        data: '已过期',
+        value: 4,
+        status: 'warning',
+        text: '已过期',
+        key: '4',
+      }
+    ],
 
     viewId: '', // 任务模型列表点击查看的id
     selectParamsId: '', // 根据前置后置任务id查询参数的id
@@ -83,41 +111,53 @@ const SeqModel = {
   },
   effects: {},
   reducers: {
-    getTaskDetail(state, { payload }) {
+    getTaskDetail(state, {
+      payload
+    }) {
       return {
         ...state,
         taskDetail: payload,
       };
     },
-    setEditTaskModelId(state, { payload }) {
+    setEditTaskModelId(state, {
+      payload
+    }) {
       return {
         ...state,
         editTaskModelId: payload, // 任务模型修改时候传的任务id, 查询参数列表
       };
     },
     // 参数数据
-    getArgumentsList(state, { payload }) {
+    getArgumentsList(state, {
+      payload
+    }) {
       console.log(payload);
       return {
         ...state,
         argumentList: payload,
       };
     },
-    setViewId(state, { payload }) {
+    setViewId(state, {
+      payload
+    }) {
       console.log(payload);
       return {
         ...state,
         viewId: payload,
       };
     },
-    setViewParamsId(state, { payload }) {
+    setViewParamsId(state, {
+      payload
+    }) {
       console.log(payload);
       return {
         ...state,
         selectParamsId: payload,
       };
     },
-    getEditOriginModelData(state, { payload }) {
+    getEditOriginModelData(state, {
+      payload
+    }) {
       return {
         ...state,
         editOriginModelData: payload,
