875602771a3a7fb04e21fcbee110826145d4e9c9
niko <232641655@qq.com>
2020-04-26 19:24:01 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { formatMessage } from 'umi/locale';
 
 const baseURLMap = {
   dev: 'https://devapi.sangon.com:30443/api',
-  test: 'https://testapi.sangon.com:8443/api',
+  test: 'https://testapi.sangon.com/api',
   pre: 'https://preapi.sangon.com/api',
   prod: 'https://api.sangon.com/api',
 };
