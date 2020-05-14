d858a4aeab56b8e6f05b494b0de813e63a204012
niko <232641655@qq.com>
2020-05-14 15:10:12 +0800

    shilei_v0.0.2_process_过期状态的修改

--- src/pages/project/process-model/model.js
+++ src/pages/project/process-model/model.js
@@ -27,6 +27,37 @@ const projectModel = {
     // moveGroup: [],
     processDetail: {},
     // processList: [],
+    // 状态
+    status: [
+      // 未发布
+      {
+        value: 1,
+        text: '未发布',
+        // i18n: 'bp.verfication',
+        status: 'default',
+      },
+      // 已发布
+      {
+        value: 2,
+        text: '已发布',
+        // i18n: 'bp.completed',
+        status: 'success',
+      },
+      // 已禁用
+      {
+        value: 3,
+        text: '已禁用',
+        // i18n: 'bp.rejected',
+        status: 'error',
+      },
+      // 已过期
+      {
+        value: 4,
+        text: '已过期',
+        // i18n: 'bp.expired',
+        status: 'warning',
+      },
+    ],
   },
   effects: {
     *setValue({ payload, key }, { put }) {
